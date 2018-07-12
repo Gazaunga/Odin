4c80cecccb1b2617fcd98d06f2b7b7bb92105d67
Gazaunga <Gazaunga@gmail.com>
2018-07-12 15:15:09 -0400

    Update caesar_cipher.rb

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -3,6 +3,12 @@
 ## TODO:
 # Make into function/commandline application
 
-alphabet = ("a".."z").to_a
-index = gets.chomp.to_i
-puts alphabet.rotate(index)
+#alphabet = ("a".."z").to_a
+#index = gets.chomp.to_i
+#puts alphabet.rotate(index)
+
+str = gets.chomp.downcase.chars
+alphabet = str.to_a
+index = (1...27).to_a
+c = Hash[index.zip(alphabet)]
+puts c
